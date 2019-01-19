6c5ba6b34b917ff8f10f8f78a3192c3c205b8e6f
rabobadilla <raul.bobadilla.mendez@hotmail.com>
2019-01-19 16:15:32 -0500

    Add files via upload

---
+++ 3. Test/1. Unit Tests/MAS.TEST/Clases/UnitTestEmployee.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// File for the test
+/// </summary>
+/// <remarks>
+/// Autor: Raul Bobadilla - raul.bobadilla.mendez@hotmail.com
+/// Fecha: 18/01/2018
+/// </remarks>
+namespace MAS.TEST.Clases
+{
+    using MAS.BL.Classes;
+    using MAS.CI.Enumerations;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+
+    /// <summary>
+    /// Main class for the tests
+    /// </summary>
+    [TestClass]
+    public class UnitTestEmployee
+    {
+        /// <summary>
+        /// Get the type of contract
+        /// </summary>
+        [TestMethod]
+        public void GetContractTypeName()
+        {
+            EmployeeTest employeeTest = new EmployeeTest();
+            EmployeeBL employeeBL = new EmployeeBL();
+            string typeContractExpected = TypesContracts.HourlySalaryEmployee.ToString();
+
+            employeeTest.id = 1;
+
+            string typeContract = employeeBL.GetEmployees(employeeTest).First().contractTypeName;
+
+            Assert.AreEqual(typeContractExpected, typeContract);
+        }
+    }
+}
