d1b1a8244e3a53116e1e2789cb229f316b6e3724
rabobadilla <raul.bobadilla.mendez@hotmail.com>
2019-01-19 16:19:37 -0500

    Add files via upload

---
+++ 2. Back End/1. Web API/MAS.WEBAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// File for the controller employee
+/// </summary>
+/// <remarks>
+/// Autor: Raul Bobadilla - raul.bobadilla.mendez@hotmail.com
+/// Fecha: 18/01/2018
+/// </remarks>
+namespace MAS.WEBAPI.Controllers
+{
+    using MAS.BL.Classes;
+    using MAS.CI.Extensions;
+    using MAS.CI.Interfaces;
+    using MAS.WEBAPI.Models;
+    using MAS.WEBAPI.UtilitiesUI;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Class for employee's controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        #region ""PUBLIC FUNCTIONS"
+
+        /// <summary>
+        /// Get all the employees
+        /// </summary>
+        /// <param name="id">Id of the employee</param>
+        /// <returns>An Action result</returns>
+        [HttpGet("{id}")]
+        [Produces("Application/json")]
+        public ActionResult<IEnumerable<IEmployeeDTO>> GetEmployee(int id)
+        {
+            return this.ExecuteWrapperUI<EmployeeController>(() =>
+            {
+                EmployeeBL employeeBL = new EmployeeBL();
+                List<IEmployeeDTO> employees = employeeBL.GetEmployees(new EmployeeModel() { id = id });
+
+                return Ok(employees);
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ 2. Back End/1. Web API/MAS.WEBAPI/UtilitiesUI/WrapperUI.cs
@@ -0,0 +1,45 @@
+﻿/// <summary>
+/// File for execution wrapper in the rest api
+/// </summary>
+/// <remarks>
+/// Autor: Raul Bobadilla - raul.bobadilla.mendez@hotmail.com
+/// Fecha: 18/01/2018
+/// </remarks>
+namespace MAS.WEBAPI.UtilitiesUI
+{
+    using MAS.UTILITIES.Utilities;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Net;
+
+    /// <summary>
+    /// Wrapper for the rest api
+    /// </summary>
+    public static class WrapperUI
+    {
+        #region ""PUBLIC FUNCTIONS"
+
+        /// <summary>
+        /// Wrapper for execute the function
+        /// </summary>
+        /// <typeparam name="C">Executed Class</typeparam>
+        /// <param name="controller">Controller of api rest</param>
+        /// <param name="bodyToExecute">Body to execute</param>
+        /// <returns>A function</returns>
+        public static ActionResult ExecuteWrapperUI<C>(this ControllerBase controller, Func<ActionResult> bodyToExecute) where C : class
+        {
+            ActionResult respuesta;
+
+            respuesta = Wrapper.ExecuteWrapper<ActionResult, C>(bodyToExecute);
+
+            if (respuesta == null)
+            {
+                respuesta = controller.StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+
+            return respuesta;
+        }
+
+        #endregion
+    }
+}
